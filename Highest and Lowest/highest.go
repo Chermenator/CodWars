673d9958c3e95430578acf4177745d561df61a14
Chermen Laliev <appolon14092002@gmail.com>
2021-04-16 18:44:52 +0300

    added 1 kata

--- Highest and Lowest/highest.go
+++ Highest and Lowest/highest.go
@@ -1,6 +1,42 @@
 package highest
 
-func HighAndLow(in string) string {
+import (
+	"fmt"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+)
 
-	return "throw towel"
+func HighAndLow(in string) string {
+	max, min := Slise(in)
+
+	return fmt.Sprintf("%d %d", max, min)
+}
+
+func Slise(inp string) (int32, int32) {
+	inp = strings.TrimSpace(inp)
+
+	var inpSliese []string = strings.Split(inp, " ")
+
+	max_num := int32(math.MinInt32)
+	min_num := int32(math.MaxInt32)
+
+	//var numbers []int32
+	for i := 0; i < len(inpSliese); i++ {
+		num, err := strconv.Atoi(inpSliese[i])
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		//numbers[i] = int32(num)
+		if int32(num) >= max_num {
+			max_num = int32(num)
+		}
+		if int32(num) < min_num {
+			min_num = int32(num)
+		}
+	}
+	return max_num, min_num
 }
